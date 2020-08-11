e9966656b758f4e9d6a744330591b342bc360a33
Anton Strogonoff <anton@strogonoff.name>
2020-08-12 00:25:09 +0900

    chore(demo): workaround for Chinese language code

--- src/components/isoLang.ts
+++ src/components/isoLang.ts
@@ -1,5 +1,8 @@
 import iso639_2_data from 'iso-639-2'
 
 export function getLanguageTitleFrom6392BorT(code: string): string | null {
-  return iso639_2_data.find(i => i.iso6392T === code || i.iso6392B === code)?.name
+  const effectiveCode = code !== 'chn' ? code : 'zho'
+  return iso639_2_data.find(i =>
+    i.iso6392T === effectiveCode || i.iso6392B === effectiveCode
+  )?.name
 }
