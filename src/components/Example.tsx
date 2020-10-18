8a39453f02e7274f5328d059989b3d51a3960196
webdev778 <webdev778@gmail.com>
2020-10-18 15:32:57 +0800

    refactor components

--- src/components/Example.tsx
+++ src/components/Example.tsx
@@ -1,11 +1,56 @@
 import React from 'react'
+import { ScriptConversionExample } from "types/index";
 
-export const Example:React.FC<{
-    samples: any
+export const English: React.FC<{
+  samples: any
 }> = function ({ samples }) {
-    return (
-        <>
-            { !!samples && <span> [<i>{samples}</i>]</span> }
-        </>
-    )
+  return (
+    <>
+      { !!samples && <span> [<i>{samples}</i>]</span>}
+    </>
+  )
+}
+
+export const Sample: React.FC<{
+  data: ScriptConversionExample
+}> = function ({ data: s }) {
+  return (
+    <div>
+      <p>
+        <strong style={{ color: '#002060' }}>{s.lang}</strong> [{s.isoName}]
+      </p>
+      <p>
+        {s.samples.map((e: string, i: number) => (
+          <span key={i}>
+            { `${e} ${s.result[i] ? s.result[i] : ''}`}
+            { s.english && !!s.english[i] && <English samples={s.english[i]} />}
+            { i + 1 < s.samples.length && ', '}
+          </span>
+        ))
+        }
+        <i>{(s.result.length > 0) ? '' : ' (To be implemented)'}</i>
+      </p>
+    </div>
+  )
+}
+
+export const Poster: React.FC<{
+  data: ScriptConversionExample[]
+}> = function ({ data }) {
+  return (
+    <div style={{ display: 'flex' }}>
+      <div style={{ flex: 1 }}>
+        {data.slice(0, data.length / 2).map((s: ScriptConversionExample, i: number) => (
+          <Sample data={s} key={i} />
+        ))
+        }
+      </div>
+      <div style={{ flex: 1 }}>
+        {data.slice(data.length / 2, data.length).map((s: ScriptConversionExample, i: number) => (
+          <Sample data={s} key={i} />
+        ))
+        }
+      </div>
+    </div>
+  )
 }
--- src/containers/Landing.tsx
+++ src/containers/Landing.tsx
@@ -7,7 +7,7 @@ import Loader from 'react-loader-spinner'
 
 import samples from './samples.json'
 import bgn from './bgnpcgn.json'
-import { Example } from '../components/Example'
+import { Poster } from 'components/Example'
 
 export default () => {
   const { repoInfo }: { readmeSections: ReadmeSection[], repoInfo: RepoInfo, mapsInfo: any } =
@@ -112,48 +112,7 @@ export default () => {
             >
               <h2>{ `UN systems`}</h2>
               <p><i>{ `Each title of a language or a writing system is followed by a note on the appropriate romanization system used (UN = United Nations, BGN/PCGN = US Board on Geographic Names and Permanent Committee on Geographical Names for British Official Use)`}</i></p>
-                    <div style={{display: 'flex'}}>
-                      <div style={{flex: 1}}>
-                        { sampleData.slice(0, sampleData.length/2).map((s,index) => (
-                            <div key={index}>
-                              <p> <strong style={{color: '#002060'}}>{s.lang}</strong> [{s.isoName}]
-                              </p>
-                              <p>
-                                { s.samples.map((e: any, i: number) => (
-                                    <span key={i}>
-                                      { `${e} ${s.result[i] ? s.result[i] : ''}` }
-                                      { s.english && !!s.english[i] && <Example samples={s.english[i]} /> }
-                                      { i+1 < s.samples.length && ', ' }
-                                    </span>
-                                ))
-                                }
-                                <i>{s.systemName ? '' : ' (To be implemented)'}</i>
-                              </p>
-                            </div>
-                          ))
-                        }
-                      </div>
-                      <div style={{flex: 1}}>
-                        { sampleData.slice(sampleData.length/2, sampleData.length).map((s,index) => (
-                            <div key={index}>
-                              <p> <strong style={{color: '#002060'}}>{s.lang}</strong> [{s.isoName}]
-                              </p>
-                              <p>
-                                { s.samples.map((e: any, i: number) => (
-                                    <span key={i}>
-                                      { `${e} ${s.result[i] ? s.result[i] : ''} ` }
-                                      { s.english && !!s.english[i] && <Example samples={s.english[i]} /> }
-                                      { i+1 < s.samples.length && ', ' }
-                                    </span>
-                                ))
-                                }
-                                <i>{s.systemName ? '' : ' (To be implemented)'}</i>
-                              </p>
-                            </div>
-                          ))
-                        }
-                    </div>
-                  </div>
+              <Poster data={sampleData} />
             </Section>
             <Section
                 key={'bgnpcgn-example'}
@@ -161,46 +120,7 @@ export default () => {
             >
               <h2>{ `BGN/PCGN systems`}</h2>
               <p><i>{ `Each title of a language or a writing system is followed by a note on the appropriate romanization system used (UN = United Nations, BGN/PCGN = US Board on Geographic Names and Permanent Committee on Geographical Names for British Official Use)`}</i></p>
-                    <div style={{display: 'flex'}}>
-                      <div style={{flex: 1}}>
-                        { bgnData.slice(0, bgnData.length/2).map((s,index) => (
-                            <div key={index}>
-                              <p> <strong style={{color: '#002060'}}>{s.lang}</strong> [{s.isoName}]
-                              </p>
-                              <p>
-                                { s.samples.map((e: any, i: number) => (
-                                    <span key={i}>
-                                      { `${e} ${s.result[i] ? s.result[i] : ''} ` }
-                                      { i+1 < s.samples.length && ', ' }
-                                    </span>
-                                  ))
-                                }
-                                <i>{ s.todo ? ' (To be implemented)' : '' }</i>
-                              </p>
-                            </div>
-                          ))
-                        }
-                      </div>
-                      <div style={{flex: 1}}>
-                        { bgnData.slice(bgnData.length/2, bgnData.length).map((s,index) => (
-                            <div key={index}>
-                              <p> <strong style={{color: '#002060'}}>{s.lang}</strong> [{s.isoName}]
-                              </p>
-                              <p>
-                                { s.samples.map((e: any, i: number) => (
-                                    <span key={i}>
-                                      { `${e} ${s.result[i] ? s.result[i] : ''} ` }
-                                      { i+1 < s.samples.length && ', ' }
-                                    </span>
-                                ))
-                                }
-                                <i>{ s.todo ? ' (To be implemented)' : '' }</i>
-                              </p>
-                            </div>
-                          ))
-                        }
-                    </div>
-                  </div>
+              <Poster data={bgnData} />
             </Section>
           </>
         }
