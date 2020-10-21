718e9e3fe7a8c4d2760bce45aabae61ecb244047
Ronald Tse <ronald.tse@ribose.com>
2020-10-22 03:00:20 +0800

    Remove old ODNI systems

--- src/pages/un.tsx
+++ src/pages/un.tsx
@@ -5,6 +5,6 @@ export default () => (
   <>
     <h2>{`UN systems`}</h2>
     <p><i>{`Each title of a language or a writing system is followed by a note on the appropriate romanization system used (UN = United Nations, BGN/PCGN = US Board on Geographic Names and Permanent Committee on Geographical Names for British Official Use)`}</i></p>
-    <ExamplePage sampleName="samples"/>
+    <ExamplePage sampleName="un"/>
   </>
 )
