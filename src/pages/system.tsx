6847903c71958ccf9a6de7b5cbdd5b34a48d4a58
Ohm0n <thecyberd3m0n@gmail.com>
2021-06-19 11:54:48 +0200

    filtering

--- src/pages/system.tsx
+++ src/pages/system.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouteData } from 'react-static';
 import { Link } from '@reach/router';
 import styled from 'styled-components';
@@ -61,14 +61,17 @@ const Heading = styled.h1`
 `
 
 const SystemList = () => {
+  const [ filter, setFilter ] = useState('');
   const {
     mapsInfo,
   }: {
     mapsInfo: any;
   } = useRouteData();
-
+  
   console.log(mapsInfo);
-  const list = mapsInfo.data.sort().map((system: string) => (
+  const list = mapsInfo.data.sort().filter((x: string) => {
+    return x.includes(filter);
+  }).map((system: string) => (
     <li>
       <Link to={`/system/${system}`} key={system}>
         {system}
@@ -84,7 +87,7 @@ const SystemList = () => {
         System List: {mapsInfo.meta.total}
       </Heading>
         <Form>
-        <TextBox placeholder="Search" />
+        <TextBox onChange={e => setFilter(e.target.value)} placeholder="Search" />
         <SearchButton>
           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
