bd7b728f8b767f33c8ae96e2ec0edc173243886b
webdev778 <webdev778@gmail.com>
2021-06-20 17:26:40 +0900

    update total size on filtering

--- src/pages/system.tsx
+++ src/pages/system.tsx
@@ -5,11 +5,12 @@ import styled from 'styled-components';
 const Form = styled.form`
   outline: 0;
   float: left;
-  -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+  -webkit-box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12),
+    0 1px 2px rgba(0, 0, 0, 0.24);
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
   -webkit-border-radius: 4px;
   border-radius: 4px;
-`
+`;
 const TextBox = styled.input`
   outline: 0;
   height: 42px;
@@ -22,8 +23,7 @@ const TextBox = styled.input`
   float: left;
   -webkit-border-radius: 4px 0 0 4px;
   border-radius: 4px 0 0 4px;
-
-`
+`;
 
 const SearchButton = styled.button`
   outline: 0;
@@ -35,16 +35,16 @@ const SearchButton = styled.button`
   text-align: center;
   line-height: 42px;
   border: 0;
-  color: #FFF;
+  color: #fff;
   font: normal normal normal 14px/1 FontAwesome;
   font-size: 16px;
   text-rendering: auto;
   text-shadow: 0 1px 1px rgba(0, 0, 0, 0.2);
-  -webkit-transition: background-color .4s ease;
-  transition: background-color .4s ease;
+  -webkit-transition: background-color 0.4s ease;
+  transition: background-color 0.4s ease;
   -webkit-border-radius: 0 4px 4px 0;
   border-radius: 0 4px 4px 0;
-`
+`;
 
 const SearchHeader = styled.div`
   display: flex;
@@ -54,45 +54,54 @@ const SearchHeader = styled.div`
   display: flex;
   place-content: space-between;
   margin-top: 24px;
-`
+`;
 
 const Heading = styled.h1`
   margin: 0;
-`
+`;
 
 const SystemList = () => {
-  const [ filter, setFilter ] = useState('');
+  const [filter, setFilter] = useState('');
   const {
     mapsInfo,
   }: {
     mapsInfo: any;
   } = useRouteData();
-  
-  console.log(mapsInfo);
-  const list = mapsInfo.data.sort().filter((x: string) => {
-    return x.includes(filter);
-  }).map((system: string) => (
-    <li>
-      <Link to={`/system/${system}`} key={system}>
-        {system}
-      </Link>
-    </li>
-  ));
+
+  const list: string[] = mapsInfo.data
+    .sort()
+    .filter((x: string) => {
+      return !filter || x.includes(filter);
+    })
+    .map((system: string) => (
+      <li>
+        <Link to={`/system/${system}`} key={system}>
+          {system}
+        </Link>
+      </li>
+    ));
 
   return (
     <div>
       <Link to='/'>Home</Link>
       <SearchHeader>
-      <Heading>
-        System List: {mapsInfo.meta.total}
-      </Heading>
+        <Heading>System List: {list.length}</Heading>
         <Form>
-        <TextBox onChange={e => setFilter(e.target.value)} placeholder="Search" />
-        <SearchButton>
-          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-          </svg>
-        </SearchButton>
+          <TextBox
+            onChange={(e) => setFilter(e.target.value)}
+            placeholder='Search'
+          />
+          <SearchButton>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='currentColor'
+              viewBox='0 0 16 16'
+            >
+              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+            </svg>
+          </SearchButton>
         </Form>
       </SearchHeader>
       <ul>{list}</ul>
