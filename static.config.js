fba7b562eabebd36c660193a916f811bcd33d5ca
thecyberd3m0n@gmail.com <thecyberd3m0n@gmail.com>
2021-07-09 04:45:41 +0200

    Replace the broken link of Integration Ruby Guide.. with rendering adoc content instead

--- static.config.js
+++ static.config.js
@@ -129,6 +129,14 @@ export default {
       return readmeSections.find((readmeSection) => readmeSection.id === path);
     }
 
+    console.log(findReadmeSection('usage'));
+
+    const renderADocSection = (name, path) => {
+      const header = `
+      <h2><a class="anchor" aria-hidden="true" href="#${path}"><svg class="octicon octicon-link" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"/></svg></a>${name}</h2>\n'
+      `
+      return { html: header + fs.readFileSync(`tmp/docs/${path}.html`, { encoding: 'utf8'}), titleHTML: name, id: path.toLowerCase() };
+    }
     const newRoutes = [
       {
         path: "/",
@@ -279,7 +287,7 @@ export default {
           sections: [
             findReadmeSection('installation'),
             findReadmeSection('usage'),
-            findReadmeSection('integration-with-ruby-applications')
+            renderADocSection('Integration with Ruby Applications', 'Integration_with_Ruby_Applications')
           ] 
         })
       },
