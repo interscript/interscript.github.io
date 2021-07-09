d51985fdd14d4ae3df3d362f52c76283e7e0ef4c
Ohm0n <thecyberd3m0n@gmail.com>
2021-07-09 10:36:01 +0200

    added margin bottom to bottom menus

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,16 +1,20 @@
 import { Link } from "@reach/router";
 import React from "react";
 import { SectionNav } from "./SectionNav";
-import { SectionNavItem } from "./SectionNavItem";
+import styled from "styled-components";
 
 export function HeaderMenu() {
     return (
-        <>
+        <HeadMenu>
             <SectionNav>
                 <Link to="/">
                     <span>&laquo; Home</span>
                 </Link>
             </SectionNav>
-        </>
+        </HeadMenu>
     );
 }
+
+const HeadMenu = styled.section`
+    margin-bottom: 24px;
+`;
