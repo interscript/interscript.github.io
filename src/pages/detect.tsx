1d79b314f8f8e65f3e8b49926de97ff09043c328
webdev778 <webdev778@gmail.com>
2021-08-16 23:00:27 +0800

    Styling System Detection Page

--- src/pages/detect.tsx
+++ src/pages/detect.tsx
@@ -1,5 +1,8 @@
 import React, { useState, useCallback } from "react";
 import axios, { AxiosPromise } from "axios";
+import styled from "styled-components";
+import { primaryColor } from "../App";
+import { Link } from "@reach/router";
 
 const API_URI = "https://staging-api.interscript.org/staging";
 
@@ -52,25 +55,92 @@ export default () => {
 
     const list = result
         .sort((a, b) => a.distance - b.distance)
+        .slice(0, 10) // take first 10 best relevance search results
         .map((s: SystemRelevance, i) => (
             <li key={i}>
-                {s.mapName} --
-                <span>{s.distance}</span>
+                <Link to={`/systems/${s.mapName}`} key={s.mapName}>
+                    <code style={{ color: i < 3 ? "red" : "none" }}>{s.mapName}</code>
+                </Link>
             </li>
         ));
 
     return (
         <div>
             <h1> Detect Transliteration System</h1>
-            <form>
-                Input : <input type="text" onChange={(e) => setSource(e.target.value)} />
-                Output : <input type="text" onChange={(e) => setTarget(e.target.value)} />
-                <input type="button" value="Detect" onClick={detect} disabled={loading} />
-                <ul>
-                    {error && <li>{error}</li>}
-                    {list}
-                </ul>
-            </form>
+            <SampleAndResult>
+                <SampleTextArea
+                    placeholder={"Input source text"}
+                    style={{
+                        boxShadow: source.trim() === "" ? `#${primaryColor} 0 0 0px .5rem` : undefined,
+                    }}
+                    onChange={(evt) => setSource(evt.currentTarget.value)}
+                />
+                <ConvertButton
+                    onClick={detect}
+                    value="Detect"
+                    disabled={loading || source.trim() === "" || target.trim() === ""}
+                >
+                    Detect
+                </ConvertButton>
+                <ResultTextArea
+                    placeholder={"Input target text"}
+                    style={{
+                        boxShadow: target.trim() === "" ? `#${primaryColor} 0 0 0px .5rem` : undefined,
+                    }}
+                    onChange={(evt) => setTarget(evt.currentTarget.value)}
+                />
+            </SampleAndResult>
+            <ul>
+                {error && <li>{error}</li>}
+                {list}
+            </ul>
         </div>
     );
 };
+
+const ConvertButton = styled.button`
+    flex-shrink: 0;
+    margin: 0 0.5rem;
+    padding: 0.5rem 1rem;
+    border: 0;
+    font-size: 100%;
+
+    color: ${(props) => (props.disabled ? "silver" : `white`)};
+
+    background: ${(props) => (props.disabled ? "whiteSmoke" : `#${primaryColor}`)};
+`;
+
+const SampleAndResult = styled.div`
+    margin-top: 1rem;
+
+    > :first-child,
+    > :last-child {
+        width: 100%;
+    }
+
+    @media screen and (min-width: 900px) {
+        display: flex;
+        flex-flow: row nowrap;
+        align-items: flex-start;
+
+        > :first-child,
+        > :last-child {
+            flex: 1;
+            width: unset;
+        }
+    }
+`;
+
+const SampleTextArea = styled.textarea`
+    font-size: 100%;
+    padding: 0.5rem;
+    margin: 0;
+    background: whiteSmoke;
+    border: 0;
+    z-index: 2;
+    display: block;
+`;
+
+const ResultTextArea = styled(SampleTextArea)`
+    cursor: default;
+`;
