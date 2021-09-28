ab5899423c06202ba72fcde5040d6dacc3821bc9
webdev778 <webdev778@gmail.com>
2021-09-28 13:55:22 +0800

    Add stage-name onto api domain

--- src/components/LiveDemo.tsx
+++ src/components/LiveDemo.tsx
@@ -8,7 +8,7 @@ import { SystemSelector } from "./SystemSelector";
 import { primaryColor } from "../App";
 import styled from "styled-components";
 
-const API_ENDPOINT = "https://api.interscript.org"; //for issue https://github.com/interscript/infrastructure/issues/17
+const API_ENDPOINT = "https://api.interscript.org/prod"; //for issue https://github.com/interscript/infrastructure/issues/17
 // const API_ENDPOINT = "https://staging-api.interscript.org/staging";
 
 type DemoType = "RUBY" | "JAVASCRIPT";
