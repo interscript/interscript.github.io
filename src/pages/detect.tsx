interscript/interscript.github.io

commit 3a6d13ea84f220f7f55dcb97f580ca59c7c0c946

Fix api url to point to production env

src/pages/detect.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { primaryColor } from "../App";
 import { Link } from "@reach/router";
 
-const API_URI = "https://staging-api.interscript.org/staging";
+const API_URI = "https://api.interscript.org";
 
 interface SystemRelevance {
     distance: number;

src/components/LiveDemo.tsx
@@ -8,8 +8,8 @@ import { SystemSelector } from "./SystemSelector";
 import { primaryColor } from "../App";
 import styled from "styled-components";
 
-// const API_ENDPOINT = "https://api.interscript.org"; //for issue https://github.com/interscript/infrastructure/issues/17
-const API_ENDPOINT = "https://staging-api.interscript.org/staging"; //for issue https://github.com/interscript/infrastructure/issues/17
+const API_ENDPOINT = "https://api.interscript.org"; //for issue https://github.com/interscript/infrastructure/issues/17
+// const API_ENDPOINT = "https://staging-api.interscript.org/staging";
 
 type DemoType = "RUBY" | "JAVASCRIPT";
 

commit 182316c0c14727d2cff6e67b6239d684e5ea4451

Fix detect
The problem was introduced with 3a6d13ea84f220f7f55dcb97f580ca59c7c0c946.
ab5899423c06202ba72fcde5040d6dacc3821bc9 fixed it for LiveDemo, but not
for detect.

src/pages/detect.tsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { primaryColor } from "../App";
 import { Link } from "@reach/router";
 
-const API_URI = "https://api.interscript.org";
+const API_URI = "https://api.interscript.org/prod";
 
 interface SystemRelevance {
     distance: number;