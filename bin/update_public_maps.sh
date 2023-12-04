21d426221c4997d1a1f0bcc9cae27e9d3ab78775
webdev778 <webdev778@gmail.com>
2023-12-05 04:33:29 +0800

    Replace shell script to update map data with js code for cross platform

--- bin/update_public_maps.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-#Note to ensure to run yarn install prior to run this script
-#yarn install
-rm ./public/maps/*.js
-rm ./public/maps/*.json
-cp ./node_modules/interscript/src/maps/* ./public/maps
